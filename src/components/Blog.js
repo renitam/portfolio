4a07e6d00ce356156d8b0fb797a83cb311f0f0e3
Renita Mwangachuchu <renita@Renitas-MBP.fios-router.home>
2024-02-26 23:30:06 -0500

    Add new components for web dev blog and contact pop up form.

---
+++ src/components/Blog.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Blog({ featuredProject }) {
+    return (
+      <FeaturedSection id="featured">
+        <Gradient2>
+          <Gradient2A />
+          <Gradient2B />
+        </Gradient2>
+        <FeaturedProjectArticle>
+          <TagList>
+            {featuredProject.tags.map((tag, i) =>
+              <TagBubble key={i}><Tag>{tag}</Tag></TagBubble>)}
+          </TagList>
+          <Header>{featuredProject.name}</Header>
+          <Text2>{featuredProject.desc}</Text2>
+          <Link href={featuredProject.link}>
+            <CalltoAction>Tell me more &#8594;</CalltoAction>
+          </Link>
+        </FeaturedProjectArticle>
+        <FeaturedImageWrapper>
+          <FeaturedImage src={iPad} alt="iPad frame" />
+        </FeaturedImageWrapper>
+      </FeaturedSection>
+    );
+  }
+  
+  export default Blog;
